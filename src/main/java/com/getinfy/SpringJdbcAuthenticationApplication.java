2a481b22b1bb669181bf3261e72d758d27c66d26
venkat <venkat@gmail.com>
2023-09-26 15:10:50 +0530

    intial commit

---
+++ src/main/java/com/getinfy/SpringJdbcAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.getinfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcAuthenticationApplication.class, args);
+	}
+
+}
